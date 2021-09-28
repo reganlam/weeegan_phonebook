455794614b59e7676fbfe362e18e0bc7fe599cbf
Regan Lam <regan_lam@hotmail.com>
2021-09-27 23:13:54 -0700

    Fix baseurl

--- index.js
+++ index.js
@@ -177,8 +177,7 @@ const errorHandler = (error, request, response, next) => {
 // this has to be the last loaded middleware.
 app.use(errorHandler);
 
-const PORT =
-	process.env.NODE_ENV === "dev" ? process.env.DEV_PORT : process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
